62bbafc2eb0138855c25846374297ecaf312bc79
Jonathan Kleinpeter <46002837+jonkleinpet@users.noreply.github.com>
2019-02-28 09:33:50 -0600

    Delete config.js

--- src/config.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-module.exports = {
-  PORT: process.env.PORT || 8000,
-  NODE_ENV: process.env.NODE_ENV || 'development',
-  DB_URL: process.env.DB_URL || 'postgresql://jon@localhost/bookmarks',
-  TEST_DB_URL: process.env.TEST_DB_URL || 'postgresql://jon@localhost/test_bookmarks'
-};
